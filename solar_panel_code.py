45bb6b9a2ce4cd46256912aed677160ca2831bde
Rishi Reddy <rishi030807@gmail.com>
2026-01-10 23:11:41 -0500

    Add files via upload
    python code is not finished. testing github actions.

---
+++ solar_panel_code.py
@@ -0,0 +1,66 @@
+# source https://open-meteo.com/en/docs?bounding_box=-90,-180,90,180&hourly=temperature_2m,shortwave_radiation
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+import plotly.express as px
+
+def estimate_demand(row):
+    date = row['date']
+    temp = row['temperature_2m']
+    return (temp - 18) * 0.
+
+def solar_energy(row):
+    rad = row['shortwave_radiation']
+    return rad * 2
+
+
+
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 52.52,
+	"longitude": 113.41,
+	"hourly": ["temperature_2m", "shortwave_radiation"],
+}
+responses = openmeteo.weather_api(url, params=params)
+
+response = responses[0]
+print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation: {response.Elevation()} m asl")
+print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_shortwave_radiation = hourly.Variables(1).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["shortwave_radiation"] = hourly_shortwave_radiation
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+hourly_dataframe["estimated demand"] = hourly_dataframe.apply(estimate_demand, axis = 1)
+print("\nHourly data\n", hourly_dataframe)
+
+# Create the graph using Plotly 
+fig = px.line(hourly_dataframe, x='date', y='shortwave_radiation', title='solar rad')
+
+# write to html
+fig.write_html("index.html")
