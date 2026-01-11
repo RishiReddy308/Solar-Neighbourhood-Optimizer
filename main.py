6921e5ec21aa41bcda21dc44f7734f948d1c6eb7
Rishi Reddy <rishi030807@gmail.com>
2026-01-11 08:48:52 -0500

    Refactor energy estimation and update output graphs
    Refactor solar energy estimation and demand calculation. Update output graphs to show excess energy and adjust HTML output.

--- main.py
+++ main.py
@@ -7,15 +7,62 @@ from retry_requests import retry
 
 import plotly.express as px
 
+def excess_energy(row):
+    return row['Solar Energy'] - row['Estimated Demand']
+
+def solar_energy_output(row):
+    rad = row['Shortwave Radiation']
+    effiency = 0.2
+    coverage = 30.0 #m^2
+
+    return (rad / 1000) * effiency * coverage
+
+
 def estimate_demand(row):
-    date = row['date']
-    temp = row['temperature_2m']
-    return (temp - 18) * 0.
+    hour = row['date'].hour
+    day_of_week = row['date'].dayofweek
+    outside_temp = row['Temperature']
+    target_temp = 21.0 #celcius
+    base_demand = 0.3   #kwh always used by home
+    hvac_demand = 0.0
+    
+    temp_diff = outside_temp - target_temp
 
-def solar_energy(row):
-    rad = row['shortwave_radiation']
-    return rad * 2
+    #cooling
+    if temp_diff > 0:
+        hvac_demand = temp_diff * 0.08
 
+    #heating
+    elif temp_diff < 0:
+        hvac_demand = abs(temp_diff) * 0.01
+
+    #weekends
+    if day_of_week >= 5:
+        #waking up energy use
+        if 9 <= hour <= 11: 
+            base_demand += 0.8
+        #casual energy use
+        elif 12 <= hour <= 17:
+            base_demand += 0.6 
+        #evening use
+        elif 18 <= hour <= 23:
+            base_demand += 1.8
+
+    #weekdays
+    else:
+        #mornings
+        if 7 <= hour <= 8:
+            base_demand += 1.0
+        # no one is home at this time
+        elif 9 <= hour <= 16:
+            base_demand += 0.0 
+        # Evenings
+        elif 17 <= hour <= 22:
+            base_demand += 1.5
+
+        
+    return hvac_demand + base_demand
+    
 
 
 
@@ -28,8 +75,10 @@ openmeteo = openmeteo_requests.Client(session = retry_session)
 # The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
+    #milton  43.47, -79.87
+    #aus    25.27,79.87
 	"latitude": 43.47,
-	"longitude": -79.41,
+	"longitude": -79.87,
 	"hourly": ["temperature_2m", "shortwave_radiation"],
 }
 responses = openmeteo.weather_api(url, params=params)
@@ -51,17 +100,24 @@ hourly_data = {"date": pd.date_range(
 	inclusive = "left"
 )}
 
-hourly_data["temperature_2m"] = hourly_temperature_2m
-hourly_data["shortwave_radiation"] = hourly_shortwave_radiation
+hourly_data["Temperature"] = hourly_temperature_2m
+hourly_data["Shortwave Radiation"] = hourly_shortwave_radiation
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
 
-hourly_dataframe["estimated demand"] = hourly_dataframe.apply(estimate_demand, axis = 1)
+hourly_dataframe["Estimated Demand"] = hourly_dataframe.apply(estimate_demand, axis = 1)
+hourly_dataframe["Solar Energy"] = hourly_dataframe.apply(solar_energy_output, axis = 1)
+hourly_dataframe["Excess"] = hourly_dataframe.apply(excess_energy, axis = 1)
+
+print("\nHourly data\n", hourly_dataframe)
+
+
 
 # Create the graph using Plotly 
-fig = px.line(hourly_dataframe, x = 'date', y = 'shortwave_radiation', title = 'solar rad')
-fig2 = px.line(hourly_dataframe, x = 'date', y = 'temperature_2m', title = 'temp')
-
+fig = px.bar(hourly_dataframe, x = 'date', y = 'Excess', title = 'Net Energy (Produced - Demand)',labels={'Excess': 'Energy (kWh)'}, color='Excess', color_continuous_scale='RdYlGn')
+fig.add_hline(y=0, line_dash="solid", line_color="black")
+fig2 = px.line(hourly_dataframe, x = 'date', y = ['Solar Energy','Estimated Demand'], title = 'Solar Energy and House Demand',labels={'value': 'Energy (kWh)', 'variable': 'Source'})
+fig2.add_hline(y=0, line_dash="solid", line_color="black")
 
 f = open('index.html', 'w')
 
@@ -69,7 +125,9 @@ f = open('index.html', 'w')
 f.write("<html><head><title>Solar Dashboard</title></head><body>")
 
 # write main title
-f.write("<h1 style= 'text-align: center; '> Daily Solar & Temperature Forecast</h1>")
+f.write("<h1 style= 'text-align: center; '> Excess Energy From Solar Panels Forecast</h1>")
+f.write("<h1 style= 'text-align: center; '> Best Time to Use Electricity</h1>")
+
 
 #write graphs
 #cdn to draw the graphs
@@ -77,3 +135,5 @@ f.write(fig.to_html(full_html = False, include_plotlyjs = 'cdn'))
 f.write(fig2.to_html(full_html = False, include_plotlyjs = 'cdn'))
 
 f.write("</body></html>")
+
+print("done")
